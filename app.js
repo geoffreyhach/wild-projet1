341d5334af3c8c6f2efc9506f41d317f32db74b7
Rosecore67 <cr67.rose@gmail.com>
2022-09-28 14:54:23 +0200

    2 commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector("nav");
+const toggleButton = document.querySelector(".navbartoggle");
+
+toggleButton.addEventListener("click",function(){
+    navBar.classList.toggle("active")
+});
