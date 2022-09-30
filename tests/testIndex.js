4acb32dc50e54f244f496fd2600cf8c9fe8d5e0d
Rosecore67 <cr67.rose@gmail.com>
2022-09-30 09:57:41 +0200

    testIndex.js verification page formation

--- tests/testIndex.js
+++ tests/testIndex.js
@@ -6,3 +6,6 @@ w3cHtmlValidator.validate(options).then(w3cHtmlValidator.reporter);
 //Verification page Hobbie
 const optionsHobbie = { filename: "../hobbies.html" };
 w3cHtmlValidator.validate(optionsHobbie).then(w3cHtmlValidator.reporter);
+//Verification Formation
+const optionsFormation = { filename: "../formation.html" };
+w3cHtmlValidator.validate(optionsHobbie).then(w3cHtmlValidator.reporter);
