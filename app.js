b508f047f1d08fd69ae979616a224f503fdd2ee7
Roblin-Blondeel-Josue <rblondeeljosue@gmail.com>
2022-09-30 11:59:41 +0200

    Ajout de la fonction Pub Pop'up

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const toggleButton = document.querySelector(".navbartoggle");
 toggleButton.addEventListener("click", function () {
     navBar.classList.toggle("active");
 });
+// Pop'up de Pub
+
+const listePub = ["pub1.png", "Pub3"]
+//Preparation des pub aléatoire
+//const indexAl = Math.floor(Math.random() * (10 - 0 +1)) + 0;
+//console.log(indexAl);
+
+    window.onload = (event) => {
+        console.log('La page est complètement chargée');
+        window.open(listePub[0], "Pub Codesand'wich", "left=50%,top=50%,width=800px,height=250px");
+  };
