fd4a00c3d71781f78e8ff8c573bc40fbb08c3e19
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-10-02 18:36:55 +0200

    add js nav bar experience

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const toggleButton = document.querySelector(".navbartoggle");
 toggleButton.addEventListener("click", function () {
     navBar.classList.toggle("active");
 });
+
+
+//Yann
+
+const btn = document.querySelector(".date1");
+const textEmplacement = document.querySelector(".text--experience");
+const textDate = [
+    ["text date 1"],
+    ["text date 2"],
+    ["text date 3"]
+];
+
+btn.addEventListener("click", () => {
+    textEmplacement.innerHTML = textDate[0];
+})
