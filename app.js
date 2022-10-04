e66db66ced9f2728026140ad829306f93ac744f4
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2022-10-04 11:59:16 +0200

    added email modal feature

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ const toggleButton = document.querySelector(".navbartoggle");
 toggleButton.addEventListener("click", function () {
     navBar.classList.toggle("active");
 });
+
+//modal email
+const openBtn = document.querySelector(".cta-btn");
+const dialog = document.querySelector("dialog");
+//open modal
+openBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+//send email button
+const sendBtn = document.querySelector("#send-email");
+sendBtn.addEventListener("click", () => {
+    dialog.innerHTML = `
+    <div class="sent-container">
+        <h2>Email envoyé !</h2>
+        <button id="close-dial">Fermer</button>
+    </div>
+    `;
+    const sendCloseBtn = document.querySelector("#close-dial");
+
+    sendCloseBtn.addEventListener("click", () => {
+        dialog.close();
+    });
+});
+
+//close modal btn
+const closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+});
