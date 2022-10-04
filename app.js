dacd1a409dec8bffa16ede5ea7c20a9f20574b5a
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-10-04 14:17:59 +0200

    modif experience

--- app.js
+++ app.js
@@ -6,17 +6,3 @@ toggleButton.addEventListener("click", function () {
     navBar.classList.toggle("active");
 });
 
-
-//Yann
-
-const btn = document.querySelector(".date1");
-const textEmplacement = document.querySelector(".text--experience");
-const textDate = [
-    ["text date 1"],
-    ["text date 2"],
-    ["text date 3"]
-];
-
-btn.addEventListener("click", () => {
-    textEmplacement.innerHTML = textDate[0];
-})
